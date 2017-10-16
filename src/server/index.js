3e7d7e804949f562eef744dc73348675dd45a29c
ShamShams <shamila.remtoula@hotmail.fr>
2017-10-16 17:19:21 +0200

    Connect mongoose

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,10 @@ const config = require('./config/config');
 
 const app = express();
 
+mongoose.connect(config.mongoUrl, () => {
+  console.log('DB initialized...');
+});
+
 app.listen(config.port, () => {
   console.log(`Connected on port ${config.port}`)
 });
